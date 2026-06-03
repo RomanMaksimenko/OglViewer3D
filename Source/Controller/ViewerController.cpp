d4087be6745ba51ca00151b280954617816ae484
Roman Maksimenko <08ax3roman@gmail.com>
2026-06-03 20:36:47 +0300

    Добавлен интерфейс IViewObserver для контроллера

---
+++ Source/Controller/ViewerController.cpp
@@ -0,0 +1,47 @@
+﻿#include "ViewerController.h"
+
+
+struct IView
+{
+};
+
+struct IDocument
+{
+};
+
+//------------------------------------------------------------------------------
+/**
+*/
+//---
+ViewerController::ViewerController() = default;
+
+ViewerController::~ViewerController() = default;
+
+//------------------------------------------------------------------------------
+/**
+   Обработка события движение модели
+*/
+//---
+void ViewerController::MoveModel(Direction dir, float delta)
+{
+}
+
+
+//------------------------------------------------------------------------------
+/**
+   Обработка события вращения модели
+*/
+//---
+void ViewerController::RotateModel(Axis axis, float angle)
+{
+}
+
+
+//------------------------------------------------------------------------------
+/**
+   Обработка события масштабирования модели
+*/
+//---
+void ViewerController::ScaleModel(float scale)
+{
+}
