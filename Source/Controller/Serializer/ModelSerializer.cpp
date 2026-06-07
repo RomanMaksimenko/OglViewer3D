7b104887218af7405eb481cb4279f2606c405342
Roman Maksimenko <08ax3roman@gmail.com>
2026-06-07 15:27:05 +0300

    Добавлены методы ModelSerializer

--- Source/Controller/Serializer/ModelSerializer.cpp
+++ Source/Controller/Serializer/ModelSerializer.cpp
@@ -1,2 +1,22 @@
 ﻿#include "ModelSerializer.h"
 
+//------------------------------------------------------------------------------
+/**
+   Сериализация модели
+*/
+//---
+bool ModelSerializer::serialize(const IModel & model, std::ostream & out)
+{
+  return false;
+}
+
+
+//------------------------------------------------------------------------------
+/**
+   Десериализация модели
+*/
+//---
+std::unique_ptr<IModel> ModelSerializer::deSerialize(std::istream & in)
+{
+  return nullptr;
+}
