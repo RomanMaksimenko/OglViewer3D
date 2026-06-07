bc04dd237aff64e71c7ef4a01f528d9cbd5201d4
Roman Maksimenko <08ax3roman@gmail.com>
2026-06-07 15:54:32 +0300

    добавлена реализация ModelService

---
+++ Source/Controller/ModelService/ModelService.h
@@ -0,0 +1,32 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+//
+/// Controller/ModelService/ModelService.h содержит клас, отвечающий за чтение/запись модели
+//
+////////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+#include "Controller/IModelService.h"
+#include "Model/IModelSerializer.h"
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+/// Файл, реализующий логику чтения/записи модели
+/**
+*/
+////////////////////////////////////////////////////////////////////////////////
+class ModelService : public IModelService
+{
+  std::unique_ptr<IModelSerializer> m_serializer;
+
+  public:
+  ModelService();
+
+  /// IModelService
+  /// Загрузить модель с диска
+  virtual bool Load(const std::string & path) override;
+  /// Записать модель на диск
+  virtual void Save() override;
+  /// Записать модель как
+  virtual void SaveAs(const std::string & path) override;
+};
