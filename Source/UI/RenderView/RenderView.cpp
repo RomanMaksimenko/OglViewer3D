821384c06fd5014e19ef622183fda0b44224a5a2
Roman Maksimenko <08ax3roman@gmail.com>
2026-06-17 18:26:59 +0300

    Добавлен интерфейс IRenderView и заготовка RenderView

---
+++ Source/UI/RenderView/RenderView.cpp
@@ -0,0 +1,50 @@
+﻿#include "RenderView.h"
+
+#include "Controller/IModelProvider.h"
+
+
+//------------------------------------------------------------------------------
+/**
+*/
+//---
+RenderView::RenderView(QWidget * parent)
+  : QOpenGLWidget(parent)
+{
+}
+
+
+//------------------------------------------------------------------------------
+/**
+   Установить подписчика
+*/
+//---
+void RenderView::SetModelProvider(IModelProvider * modelProvider)
+{
+  m_modelProvider = modelProvider;
+}
+
+
+
+//------------------------------------------------------------------------------
+/**
+   Отрисовать сцену
+*/
+//---
+void RenderView::RenderScene()
+{
+
+}
+
+
+void RenderView::paintGL()
+{
+
+}
+void RenderView::resizeGL(int w, int h)
+{
+
+}
+void RenderView::initializeGL()
+{
+
+}
---
+++ Source/UI/RenderView/RenderView.h
@@ -0,0 +1,32 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+//
+/// UI/RenderView/RenderView.h содержит виджет с окном отрисовки модели
+/**
+*/
+////////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+#include <QOpenGLWidget>
+
+#include "UI/IRenderView.h"
+
+class RenderView : public IRenderView,
+                   QOpenGLWidget
+{
+  IModelProvider * m_modelProvider;
+
+public:
+  RenderView(QWidget * parent = nullptr);
+
+  /// IRenderView
+  /// Установить проводника модели
+  virtual void SetModelProvider(IModelProvider * modelProvider) override;
+  /// Отрисовать сцену
+  virtual void RenderScene() override;
+
+protected:
+  /// QOpenGLWidget
+  virtual void paintGL() override;
+  virtual void resizeGL(int w, int h) override;
+  virtual void initializeGL() override;
+};
