f3e6a99d962b1ba739001afd59d62d74c3fc9e49
Roman Maksimenko <08ax3roman@gmail.com>
2026-08-13 18:35:27 +0300

    Добавил класс Matrix4f

--- Source/Math/Matrix.h
+++ Source/Math/Matrix.h
@@ -6,16 +6,73 @@
 ////////////////////////////////////////////////////////////////////////////////
 #pragma once
 
-struct Vertex
+#include <cassert>
+
+
+////////////////////////////////////////////////////////////////////////
+//
+/// Вспомогательный класс для организации доступа к элементам матрицы
+/**
+*/
+////////////////////////////////////////////////////////////////////////
+template<typename T, size_t len>
+class MatrixRow
 {
-  float x;
-  float y;
-  float z;
+  T * m_row;
+
+public:
+  explicit MatrixRow(T * r) noexcept
+    : m_row(r)
+  {
+  }
+  const T & operator[](size_t index) const noexcept
+  {
+    assert(index < len);
+    return m_row[index];
+  }
+
+  T & operator[](size_t index) noexcept
+  {
+    assert(index < len);
+    return m_row[index];
+  }
 };
 
+
+////////////////////////////////////////////////////////////////////////
+//
+/// Класс матрицы веществнных чисел размерности 4 х 4
+/**
+*  Матрица хранится в row-major порядке: m_matrix[row][col].
+   Вектор-столбец v, преобразование: v' = M * v.
+   Порядок комбинирования трансформаций: M_total = M_proj * M_view * M_model.
+*/
+////////////////////////////////////////////////////////////////////////
 class Matrix4f
 {
+  float m_matrix[4][4];
 
+public:
+  Matrix4f();
+  Matrix4f(float a11, float a12, float a13, float a14, float a21, float a22, float a23, float a24, float a31, float a32,
+           float a33, float a34, float a41, float a42, float a43, float a44);
+
+  /// Операторы доступа
+  MatrixRow<float, 4> operator[](size_t index) noexcept;
+  const MatrixRow<const float, 4> operator[](size_t index) const noexcept;
+
+  /// Оператор умножения матрицы на матрицу
+  Matrix4f operator*(const Matrix4f & other) const;
+
+  /// Нулевая матрица
+  static Matrix4f Zero() noexcept
+  {
+    return Matrix4f{0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f};
+  }
+
+  /// Единичная матрица
+  static Matrix4f Identity() noexcept
+  {
+    return Matrix4f();
+  }
 };
-
-
