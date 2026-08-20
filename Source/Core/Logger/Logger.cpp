08603022d7fb7a5ae9aead06fe7b2e2ac95f9d61
Roman Maksimenko <08ax3roman@gmail.com>
2026-08-20 19:38:31 +0300

    Добавлен Logger с минимальным API

---
+++ Source/Core/Logger/Logger.cpp
@@ -0,0 +1,28 @@
+﻿#include "Logger.h"
+
+#include <Core/RW/RWUtils.h>
+
+#include <Core/Exceptions/ApplicationException.h>
+
+//------------------------------------------------------------------------------
+/**
+   Запись предупреждений
+*/
+//---
+void Core::Logger::Warning(std::string_view warning)
+{
+  std::filesystem::path logPath = std::filesystem::current_path() / "application.txt";
+  WriteFile(logPath, warning);
+}
+
+
+//------------------------------------------------------------------------------
+/**
+   Запись ошибок
+*/
+//---
+void Core::Logger::Error(const ApplicationException & error)
+{
+  std::filesystem::path logPath = std::filesystem::current_path() / "application.txt";
+  WriteFile(logPath, error.what());
+}
--- Source/Core/RW/RWUtils.h
+++ Source/Core/RW/RWUtils.h
@@ -11,3 +11,6 @@
 
 /// Прочитать файл
 std::string ReadFile(const std::filesystem::path & path);
+
+/// Записать файл
+bool WriteFile(const std::filesystem::path & path, std::string_view data);
