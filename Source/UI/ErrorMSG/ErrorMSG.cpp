20842bf6a7373f768294059a71ec793ea64d3069
Roman Maksimenko <08ax3roman@gmail.com>
2026-08-21 18:41:27 +0300

    Добавил обертку для QMessageBox для показа ошибок

---
+++ Source/UI/ErrorMSG/ErrorMSG.cpp
@@ -0,0 +1,15 @@
+﻿#include "ErrorMSG.h"
+
+#include <QMessageBox>
+#include <QString>
+
+
+//------------------------------------------------------------------------------
+/**
+   Показать сообщение
+*/
+//---
+void ErrorMessage::Show(const std::string& msg)
+{
+  QMessageBox::critical(nullptr, QObject::tr("Error"),QString::fromStdString(msg));
+}
