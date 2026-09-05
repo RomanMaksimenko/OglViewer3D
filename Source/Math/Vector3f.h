79f07a899041fde847284371ecade99602c00235
Roman Maksimenko <76163218+RomanMaksimenko@users.noreply.github.com>
2026-09-05 11:48:20 +0300

    Реализован функционал класса Vector3f (#110)

--- Source/Math/Vector3f.h
+++ Source/Math/Vector3f.h
@@ -15,10 +15,37 @@
 ////////////////////////////////////////////////////////////////////////
 struct Vector3f
 {
-  float x;
-  float y;
-  float z;
+  float x = 0.0f;
+  float y = 0.0f;
+  float z = 0.0f;
+
+  /// Операторы
+  Vector3f & operator+=(const Vector3f & other) noexcept;
+  Vector3f & operator-=(const Vector3f & other) noexcept;
+  Vector3f & operator*=(float a) noexcept;
+  Vector3f & operator/=(float a);
+
+  /// Квадрат длины
+  float LengthSquared() const noexcept;
+  /// Длина вектора
+  float Length() const noexcept;
+  /// Нормализовать вектор
+  Vector3f & Normalize();
+  /// Нормализовать вектор
+  Vector3f Normalized() const;
+  /// Скалярное произведение векторов
+  float Dot(const Vector3f & other) const noexcept;
+  /// Векторное произведение векторов
+  Vector3f Cross(const Vector3f & other) const noexcept;
 };
 
 /// Оператор сложения двух векторов
-Vector3f operator+(const Vector3f & lhs, const Vector3f & rhs);
+Vector3f operator+(Vector3f lhs, const Vector3f & rhs) noexcept;
+/// Оператор вычитания двух векторов
+Vector3f operator-(Vector3f lhs, const Vector3f & rhs) noexcept;
+/// Оператор умножения вектора на скаляр
+Vector3f operator*(Vector3f lhs, float rhs) noexcept;
+/// Оператор умножения вектора на скаляр
+Vector3f operator*(float lhs, const Vector3f & rhs) noexcept;
+/// Оператор деления вектора на скаляр
+Vector3f operator/(Vector3f lhs, float rhs);
