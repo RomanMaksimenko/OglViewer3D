9a68e8f2c3df0f6622ac37973522a7831ae6ca4d
Roman Maksimenko <08ax3roman@gmail.com>
2026-09-16 18:45:25 +0300

    Подключена библиотека stb_image

---
+++ Source/Graphics/Image.cpp
@@ -0,0 +1,142 @@
+﻿#include "Image.h"
+
+#include <Core/Exceptions/FileException.h>
+
+#include "stb_image.h"
+
+
+//------------------------------------------------------------------------------
+/**
+
+*/
+//---
+Image::~Image()
+{
+  ClearData();
+}
+
+//------------------------------------------------------------------------------
+/**
+   Конструктор перемещения
+*/
+//---
+Image::Image(Image && other) noexcept
+  : m_imageData(other.m_imageData)
+  , m_width(other.m_width)
+  , m_height(other.m_height)
+  , m_channels(other.m_channels)
+{
+  other.m_imageData = nullptr;
+  other.m_width = 0;
+  other.m_height = 0;
+  other.m_channels = 0;
+}
+
+
+//------------------------------------------------------------------------------
+/**
+   Конструктор перемещения
+*/
+//---
+Image & Image::operator=(Image && other) noexcept
+{
+  if (this != &other)
+  {
+    ClearData();
+    m_imageData = other.m_imageData;
+    m_width = other.m_width;
+    m_height = other.m_height;
+    m_channels = other.m_channels;
+
+    other.m_imageData = nullptr;
+    other.m_width = 0;
+    other.m_height = 0;
+    other.m_channels = 0;
+  }
+  return *this;
+}
+
+//------------------------------------------------------------------------------
+/**
+  Загрузить изображение
+*/
+//---
+void Image::Load(const std::string & filePath)
+{
+  ClearData();
+  stbi_set_flip_vertically_on_load(1);
+  m_imageData = stbi_load(filePath.c_str(), &m_width, &m_height, &m_channels, 0);
+  if (!m_imageData)
+    throw FileException("Cannot load texture from file " + filePath);
+}
+
+
+//------------------------------------------------------------------------------
+/**
+  Загружено ли изображение
+*/
+//---
+bool Image::IsLoaded() const noexcept
+{
+  return m_imageData != nullptr;
+}
+
+
+//------------------------------------------------------------------------------
+/**
+  Получить данные изображения
+*/
+//---
+const unsigned char * Image::Data() const
+{
+  return m_imageData;
+}
+
+
+//------------------------------------------------------------------------------
+/**
+  Получить ширину изображения
+*/
+//---
+int Image::Width() const
+{
+  return m_width;
+}
+
+
+//------------------------------------------------------------------------------
+/**
+  Получить высоту изображения
+*/
+//---
+int Image::Height() const
+{
+  return m_height;
+}
+
+
+//------------------------------------------------------------------------------
+/**
+  Получить количество каналов
+*/
+//---
+int Image::Channels() const
+{
+  return m_channels;
+}
+
+
+//------------------------------------------------------------------------------
+/**
+   Освободить ресурсы
+*/
+//---
+void Image::ClearData()
+{
+  if (m_imageData)
+    stbi_image_free(m_imageData);
+  m_imageData = nullptr;
+  m_width = 0;
+  m_height = 0;
+  m_channels = 0;
+}
